8c6f822448f5421d5b865b92224b0ca30df8414c
Naveen <p6naveen@gmail.com>
2016-06-29 15:04:33 +0530

    test cases updated

--- tests/PalindromeTest.php
+++ tests/PalindromeTest.php
@@ -13,7 +13,8 @@ require_once dirname(__DIR__) . "/vendor/autoload.php";
 class PalindromeTest extends \PHPUnit_Framework_TestCase {
 
     function testCheck() {
-        $this->assertEquals(true, \WebTools\Palindrome::Check(121));
+        $this->assertEquals(true, \WebTools\Palindrome::Check(121), "3 digit test - success");
+        $this->assertEquals(false, \WebTools\Palindrome::Check(122), "3 digit test - fail");
     }
 
 }
